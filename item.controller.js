8493efb54c3b022e718698e53f48eae1a9709462
Alex Contursi <contursi.axel@gmail.com>
2017-03-13 12:56:03 +0100

    creato una tabella con angular

--- item.controller.js
+++ item.controller.js
@@ -1,4 +1,29 @@
 // CREA UN CONTROLLER PER L'OGGETTO
+angular.module('app').controller('PrimoitemCrtl',function($scope){
+  $scope.titolo="YES";
+  $scope.titolo1="OH YES";//le variabili con $scope sono visibili sulla pagina html
+  $scope.titolo2="OOOHHH YEEEESSS!!!";
+  $scope.listaarmi=
+  [
+    {
+    id: "01",
+    nome: "GAY IMPAZIENTE",
+    descrizione:"Grande cappella di fuoco",
+    slots:"4",
+    image:"http://static.haisentito.it/haisentito/fotogallery/625X0/55457/scultura-fiammiferi-ghandi.jpg"
+  },
+  {
+    id:"02",
+    nome: "Bastone Allungati",
+    descrizione:"Un bel bastone",
+    slots:"infinito ma solo se lo tocchi",
+    image:"http://i55.tinypic.com/11kgi68.gif"
+  }];
+
+  $scope.elimina= function(id, indice){
+    console.log(id, indice)
+    $scope.listaarmi.splice(indice, 1);
+  }
 
 
 
@@ -11,3 +36,4 @@
  */
 
 //
+});
